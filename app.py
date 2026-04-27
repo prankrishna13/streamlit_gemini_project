f8cd5deb272cfd13026b35acbf1aacd48eb0edfb
Pran Krishna Bhowmik <prankrishnabhowmik13@gmail.com>
2026-04-27 17:39:03 +0600

    Update app.py

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ st.markdown("<h1 style=' text-align: justify;  '>Your personal AI (MOON) <h1/> "
 # side bar
 
 with st.sidebar:
-    #st.logo("simu.png")
+    st.logo("m.png")
     st.header("Control Panel")
     img = st.file_uploader(
         "Upload your photos",type=['jpg','png','jpeg'],
